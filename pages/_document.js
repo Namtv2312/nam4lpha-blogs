f003283f54426115913438214fb25dd66af9efe8
Trần Văn Nam <60953789+Namtv2312@users.noreply.github.com>
2022-04-09 21:58:24 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,70 +1,36 @@
-// import Document, { Html, Head, Main, NextScript } from 'next/document'
-// class MyDocument extends Document {
-//   render() {
-//     return (
-//       <Html lang="en" className="scroll-smooth">
-//         <Head>
-//           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
-//           <link
-//             rel="icon"
-//             type="image/png"
-//             sizes="32x32"
-//             href="/static/favicons/favicon-32x32.png"
-//           />
-//           <link
-//             rel="icon"
-//             type="image/png"
-//             sizes="16x16"
-//             href="/static/favicons/favicon-16x16.png"
-//           />
-//           <link rel="manifest" href="/static/favicons/site.webmanifest" />
-//           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-//           <meta name="msapplication-TileColor" content="#000000" />
-//           <meta name="theme-color" content="#000000" />
-//           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-//         </Head>
-//         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
-//           <Main />
-//           <NextScript />
-//         </body>
-//       </Html>
-//     )
-//   }
-// }
-
-// export default MyDocument
-import Script from 'next/script';
-
-function Facebook() {
-  return (
-    <div>
-      <div id="fb-root"></div>
-
-      <div id="fb-customer-chat" className="fb-customerchat"></div>
-      <Script strategy="lazyOnload">
-        {`
-            var chatbox = document.getElementById('fb-customer-chat');
-            chatbox.setAttribute("page_id", "110773384708236");
-            chatbox.setAttribute("attribution", "biz_inbox");
-      
-            window.fbAsyncInit = function() {
-              FB.init({
-                xfbml            : true,
-                version          : 'v12.0'
-              });
-            };
-      
-            (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-        `}
-      </Script>
-    </div>
-  );
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en" className="scroll-smooth">
+        <Head>
+          <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/favicons/site.webmanifest" />
+          <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#000000" />
+          <meta name="theme-color" content="#000000" />
+          <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+        </Head>
+        <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
-export default Facebook;
+export default MyDocument
+
